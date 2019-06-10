618a3cc2395b72d53c334fdf10ab18a3584d4aa2
Sandy Ludosky <sandyludosky@gmail.com>
2019-06-10 18:28:43 +0200

    initial commit

---
+++ HenriPotierSwiftTests/HenriPotierSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  HenriPotierSwiftTests.swift
+//  HenriPotierSwiftTests
+//
+//  Created by Sandy on 2019-06-10.
+//  Copyright © 2019 Sandy. All rights reserved.
+//
+
+import XCTest
+@testable import HenriPotierSwift
+
+class HenriPotierSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
